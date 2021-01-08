e60cdbe23177bad839122d5d469fd2595091d9c0
Maxim Mironenko <maxim@cloudposse.com>
2021-01-09 00:36:03 +0700

    test fixed, workflow updated

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -1,24 +1,24 @@
 variable "region" {
-  type        = "string"
+  type        = string
   description = "AWS Region"
 }
 
 variable "ssh_public_key_name" {
-  type        = "string"
+  type        = string
   description = "SSM Parameter name of the SSH public key"
 }
 
 variable "ssh_private_key_name" {
-  type        = "string"
+  type        = string
   description = "SSM Parameter name of the SSH private key"
 }
 
 variable "ssh_key_algorithm" {
-  type        = "string"
+  type        = string
   description = "SSH key algorithm to use. Currently-supported values are 'RSA' and 'ECDSA'"
 }
 
 variable "ssm_path_prefix" {
-  type        = "string"
+  type        = string
   description = "The SSM parameter path prefix"
 }
