6f784761b8d20fb32c0d32183926b2e610a4d1c3
Maxim Mironenko <maxim@cloudposse.com>
2021-02-04 14:02:15 +0700

    Terraform 0.14 upgrade (#13)
    * Terraform 0.14 upgrade
    
    * test fixed, workflow updated

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,20 +3,23 @@ provider "aws" {
 }
 
 module "kms_key" {
-  source = "git::https://github.com/cloudposse/terraform-aws-kms-key.git?ref=tags/0.7.0"
+  source  = "cloudposse/kms-key/aws"
+  version = "0.9.0"
 
-  context                 = module.this.context
   description             = "Test KMS key"
   deletion_window_in_days = 7
   enable_key_rotation     = false
+
+  context = module.this.context
 }
 
 module "ssm_tls_ssh_key_pair" {
   source               = "../../"
-  context              = module.this.context
   kms_key_id           = module.kms_key.key_id
   ssm_path_prefix      = var.ssm_path_prefix
   ssh_key_algorithm    = var.ssh_key_algorithm
   ssh_private_key_name = var.ssh_private_key_name
   ssh_public_key_name  = var.ssh_public_key_name
+
+  context = module.this.context
 }
